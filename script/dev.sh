24a7c3c85c43aca91f6810b473e825aded6517d8
André Schemaitat <a.schemaitat@gmail.com>
2022-12-19 02:36:58 +0100

    airflow with k8s executor

--- script/dev.sh
+++ script/dev.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-kubectl port-forward svc/argo-cd-argocd-server 8001:443 -n argocd
+trap killgroup SIGINT
+
+killgroup(){
+  echo "Killing $PID_ARGO !"
+  kill -INT $PID_ARGO
+  exit
+}
+
+kubectl port-forward svc/argo-cd-argocd-server 8001:443 -n argocd &
+PID_ARGO=$!
+kubectl port-forward svc/airflow-web 8002:8080 -n airflow
+# wait
