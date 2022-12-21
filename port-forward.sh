13cbb52c9aec57deb6492b180375b19663504d13
André Schemaitat <a.schemaitat@gmail.com>
2022-12-21 12:29:54 +0100

    postgres 4Gi

--- port-forward.sh
+++ port-forward.sh
@@ -4,10 +4,14 @@ trap killgroup SIGINT
 killgroup(){
   echo "Killing $PID_ARGO !"
   kill -INT $PID_ARGO
+  echo "Killing $PID_FLOWER !"
+  kill -INT $PID_FLOWER
   exit
 }
 
 kubectl port-forward svc/argo-cd-argocd-server 8001:443 -n argocd &
 PID_ARGO=$!
+kubectl port-forward svc/airflow-flower 8003:5555 -n airflow &
+PID_FLOWER=$!
 kubectl port-forward svc/airflow-web 8002:8080 -n airflow
 # wait
